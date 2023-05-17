1c505e6961fe402b92dbf59f3a898b5a802ef548
Masood-B <masoodbasardien123@gmail.com>
2023-05-17 09:43:40 +0200

    first comiit

---
+++ script/code.js
@@ -0,0 +1,17 @@
+let firstName = `Twitch`;
+console.log(typeof firstName)
+
+let firstname = 15
+console.log(typeof firstname)
+
+let statement = true
+console.log(typeof statement)
+
+let name;
+console.log(typeof name)
+
+let studentList = null
+console.log(typeof studentList)
+
+let dogType = [`Akita`, `Bullmastiff`, `St.Bernard`, `Bull Dog`, `Chihuahua`];
+console.log(dogType) 
